a13c7d5eb1f186768dfaa5dad02769ecedd9d96e
badr ghanbiE <bghaE@gmail.com>
2022-06-19 17:35:25 +0100

    add the necessary funcionality to the app

--- app/main.js
+++ app/main.js
@@ -1,21 +1,90 @@
 const skills=document.querySelectorAll('.skill-name');
 const filteredSkills=document.querySelector('.filtered-skills');
-const clearBtn=document.getElementById('clear')
+const clearBtn=document.getElementById('clear');
+const jobPost=document.querySelectorAll('.job');
+let storageOfSelectedSkills=[];
 
+// event listener
 skills.forEach(skill=>skill.addEventListener('click',(e)=>addSkill(e.target)));
 
 clearBtn.addEventListener('click',clear)
 
+filteredSkills.addEventListener('click',(e)=>removeSkillFromFilterBar(e.target))
+
+// add new selected skill to te skill filtering bar
+
 function addSkill(element){
-    let newListItem=document.createElement('li')
-     newListItem.innerHTML=`${element.innerHTML}`;
-     newListItem.classList.add('skill-name');
-     filteredSkills.appendChild(newListItem);
+    if (!storageOfSelectedSkills.includes(element.textContent)){
+        let newListItem=document.createElement('li')
+         newListItem.innerHTML=`${element.innerHTML}`;
+         newListItem.classList.add('skill-name');
+         filteredSkills.appendChild(newListItem);
+         storageOfSelectedSkills.push(newListItem.textContent);
+        updateJobs();
+    }
+}
 
-//  console.log(filteredSkills)
+  
+
+
+  function removeSkillFromFilterBar(element){
+      filteredSkills.removeChild(element);
+     storageOfSelectedSkills.pop(element.textContent);
+      updateJobs();
+      
 }
 
 
-function clear(){
-filteredSkills.innerHTML='';
+// clear the filtered skill bar and also update the jobs by showing them all
+  function clear(){
+  filteredSkills.innerHTML='';
+  jobPost.forEach(job=>job.classList.remove('hidden'));
+  storageOfSelectedSkills=[];
+  }
+
+
+  // check whether the job contains the selected skills
+  function updateJobs(){
+   jobPost.forEach(job=>{ 
+     job.classList.remove('hidden')
+   storageOfSelectedSkills.forEach(value =>{ 
+    if (!job.getAttribute('data-skills').includes(value.toLowerCase()) ){
+        job.classList.add('hidden'); 
+   } 
+   })
+  })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('front end developer css javascript'.includes('front end developer javascript css'))
+    //  function jobFilter(skill){
+    //     let skillName=skill.textContent;
+    //     jobPost.forEach(job=>{
+    //     let isContainSkill=job.getAttribute('data-skills').includes(skillName.toLowerCase());
+    //      !isContainSkill ? job.classList.add('hidden') :'';
+            
+    //     })
+    // }
