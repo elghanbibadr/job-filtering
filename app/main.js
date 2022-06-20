f8dbe941bc5f47724fd85321b3acb3f0eaef72a0
badr ghanbiE <bghaE@gmail.com>
2022-06-20 13:11:28 +0100

    organize our code using a class

--- app/main.js
+++ app/main.js
@@ -2,60 +2,141 @@ const skills=document.querySelectorAll('.skill-name');
 const filteredSkills=document.querySelector('.filtered-skills');
 const clearBtn=document.getElementById('clear');
 const jobPost=document.querySelectorAll('.job');
-let storageOfSelectedSkills=[];
+
+class JobFiltering{ 
+  constructor(){
+    this.storageOfSelectedSkills=[];
+  }
+  _addSkill(element){
+    if (!this.storageOfSelectedSkills.includes(element.textContent)){
+      let newListItem=document.createElement('li')
+       newListItem.innerHTML=`${element.innerHTML}`;
+       newListItem.classList.add('skill-name');
+       filteredSkills.appendChild(newListItem);
+       this.storageOfSelectedSkills.push(newListItem.textContent);
+      this._updateJobs();
+  }
+  }
+
+  _updateJobs(){
+    jobPost.forEach(job=>{ 
+      job.classList.remove('hidden')
+    this.storageOfSelectedSkills.forEach(value =>{ 
+     if (!job.getAttribute('data-skills').includes(value.toLowerCase()) ){
+         job.classList.add('hidden'); 
+    } 
+    })
+   })
+  }
+
+  _clear(){
+    filteredSkills.innerHTML='';
+    jobPost.forEach(job=>job.classList.remove('hidden'));
+    this.storageOfSelectedSkills=[];
+  }
+  
+  _removeSkillFromFilterBar(element){
+    filteredSkills.removeChild(element);
+    this.storageOfSelectedSkills.pop(element.textContent);
+     this._updateJobs();
+  }
+}
+
+let filterJobApp=new JobFiltering();
+
+// console.log(filterJobApp._addSkill);
 
 // event listener
-skills.forEach(skill=>skill.addEventListener('click',(e)=>addSkill(e.target)));
+skills.forEach(skill=>skill.addEventListener('click',(e)=>filterJobApp._addSkill(e.target)));
 
-clearBtn.addEventListener('click',clear)
+clearBtn.addEventListener('click',filterJobApp._clear)
 
-filteredSkills.addEventListener('click',(e)=>removeSkillFromFilterBar(e.target))
+filteredSkills.addEventListener('click',(e)=> filterJobApp._removeSkillFromFilterBar(e.target))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // add new selected skill to te skill filtering bar
 
-function addSkill(element){
-    if (!storageOfSelectedSkills.includes(element.textContent)){
-        let newListItem=document.createElement('li')
-         newListItem.innerHTML=`${element.innerHTML}`;
-         newListItem.classList.add('skill-name');
-         filteredSkills.appendChild(newListItem);
-         storageOfSelectedSkills.push(newListItem.textContent);
-        updateJobs();
-    }
-}
+// function addSkill(element){
+//     if (!storageOfSelectedSkills.includes(element.textContent)){
+//         let newListItem=document.createElement('li')
+//          newListItem.innerHTML=`${element.innerHTML}`;
+//          newListItem.classList.add('skill-name');
+//          filteredSkills.appendChild(newListItem);
+//          storageOfSelectedSkills.push(newListItem.textContent);
+//         updateJobs();
+//     }
+// }
 
   
 
 
-  function removeSkillFromFilterBar(element){
-      filteredSkills.removeChild(element);
-     storageOfSelectedSkills.pop(element.textContent);
-      updateJobs();
-      console.log(storageOfSelectedSkills,element)
+//   function removeSkillFromFilterBar(element){
+//       filteredSkills.removeChild(element);
+//      storageOfSelectedSkills.pop(element.textContent);
+//       updateJobs();
+//       console.log(storageOfSelectedSkills,element)
       
-}
+// }
 
 
-// clear the filtered skill bar and also update the jobs by showing them all
-  function clear(){
-  filteredSkills.innerHTML='';
-  jobPost.forEach(job=>job.classList.remove('hidden'));
-  storageOfSelectedSkills=[];
-  }
+// // clear the filtered skill bar and also update the jobs by showing them all
+//   function clear(){
+//   filteredSkills.innerHTML='';
+//   jobPost.forEach(job=>job.classList.remove('hidden'));
+//   storageOfSelectedSkills=[];
+//   }
 
 
-  // check whether the job contains the selected skills
-  function updateJobs(){
-   jobPost.forEach(job=>{ 
-     job.classList.remove('hidden')
-   storageOfSelectedSkills.forEach(value =>{ 
-    if (!job.getAttribute('data-skills').includes(value.toLowerCase()) ){
-        job.classList.add('hidden'); 
-   } 
-   })
-  })
-}
-
+//   // check whether the job contains the selected skills
+//   function updateJobs(){
+//    jobPost.forEach(job=>{ 
+//      job.classList.remove('hidden')
+//    storageOfSelectedSkills.forEach(value =>{ 
+//     if (!job.getAttribute('data-skills').includes(value.toLowerCase()) ){
+//         job.classList.add('hidden'); 
+//    } 
+//    })
+//   })
+// }
 
 
 
